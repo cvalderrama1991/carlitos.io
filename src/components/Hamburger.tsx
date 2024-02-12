9cb8524f9e31b3852751cc2c6c4ad63d0af4e292
Carlos <cvalderrama1991@gmail.com>
2024-02-11 19:41:38 -0500

    added label attribute to mobile nav button and changed the primary color to be lighter on dark mode

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -8,7 +8,12 @@ type Props = {
 export default function Hamburger({ setNav, nav }: Props) {
   return (
     <>
-      <button type='button' className='relative h-8 w-8 block md:hidden' onClick={() => setNav((prev) => !prev)}>
+      <button
+        type='button'
+        className='relative h-8 w-8 block md:hidden'
+        onClick={() => setNav((prev) => !prev)}
+        aria-label='mobile-nav'
+      >
         <span
           className={`bg-secondary-bg w-full h-1 absolute left-0 rounded -translate-y-[3px] transition-all ${
             nav ? 'top-[16px] rotate-45' : 'top-2 rotate-0'
